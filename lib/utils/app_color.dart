f5e352451a904a6d41e795a344869e269170edc8
khaledmamdooh <khaledalkady779@gmail.com>
2025-12-12 23:42:31 +0600

    Add app icons and some colors

--- lib/utils/app_color.dart
+++ lib/utils/app_color.dart
@@ -4,5 +4,10 @@ abstract class AppColors {
   static const Color primaryColor = Color(0xFF4C68D5);
   static const Color lightPrimaryColor = Color(0xFF959CE5);
   static const Color backgroundColor = Color(0xFFF2F2F2);
-  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color textColor = Color(0xFF6F767E);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color white80 = Color(0xFFFAFAFA);
+  static const Color black = Color(0xFF000000);
+  static const Color black80 = Color(0xFF303030);
+  static const Color red60 = Color(0xFFD93E39);
 }
---
+++ lib/utils/app_icons.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:hugeicons/hugeicons.dart';
+
+class MyAppIcons {
+  static const Widget view = HugeIcon(icon: HugeIcons.strokeRoundedView);
+  static const Widget viewOff = HugeIcon(icon: HugeIcons.strokeRoundedViewOff);
+  static const Widget email = HugeIcon(
+    icon: HugeIcons.strokeRoundedMailAtSign01,
+  );
+  static const Widget shoppingCart = HugeIcon(
+    icon: HugeIcons.strokeRoundedShoppingCart02,
+  );
+  static const Widget shoppingCartCheck = HugeIcon(
+    icon: HugeIcons.strokeRoundedShoppingCartCheckIn02,
+  );
+  static const Widget favorite = HugeIcon(
+    icon: HugeIcons.strokeRoundedFavourite,
+  );
+  static const Widget home = HugeIcon(icon: HugeIcons.strokeRoundedHome07);
+  static const Widget user = HugeIcon(icon: HugeIcons.strokeRoundedUser03);
+  static const Widget userCircle = HugeIcon(
+    icon: HugeIcons.strokeRoundedUserCircle,
+  );
+  static const Widget search = HugeIcon(icon: HugeIcons.strokeRoundedSearch01);
+  static const Widget menu = HugeIcon(icon: HugeIcons.strokeRoundedMenu11);
+  static const Widget arrowRight = HugeIcon(
+    icon: HugeIcons.strokeRoundedArrowRight01,
+  );
+  static const Widget thumbsUp = HugeIcon(
+    icon: HugeIcons.strokeRoundedThumbsUp,
+  );
+  static const Widget addCourse = HugeIcon(
+    icon: HugeIcons.strokeRoundedDashboardSquareAdd,
+  );
+  static const Widget addImage = HugeIcon(icon: HugeIcons.strokeRoundedAdd02);
+  static const Widget delete = HugeIcon(icon: HugeIcons.strokeRoundedDelete02);
+  static const Widget edit = HugeIcon(
+    icon: HugeIcons.strokeRoundedPencilEdit02,
+  );
+}
