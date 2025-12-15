ecaa4c744a0cdf9ee425809c7eff9025a595c98a
Eng-YousefAlazhari <aforsite79@gmail.com>
2025-12-15 16:51:24 +0300

    Adding bottomNavigationBar Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,25 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:yusur_app/generated/l10n.dart';
+import 'package:yusur_app/utils/app_icons.dart';
 import 'package:yusur_app/views/starting_views/widgets/customAppBar_For_StartingView.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int index = 2;
+
+  final items = <Widget>[MyAppIcons.home, MyAppIcons.favorite, MyAppIcons.user];
 
   @override
   Widget build(BuildContext context) {
@@ -24,10 +35,21 @@ class MyApp extends StatelessWidget {
       supportedLocales: S.delegate.supportedLocales,
       locale: const Locale('ar'),
 
-    home:const Scaffold(
-      appBar: CustomappbarForStartingview(),
+      home: Scaffold(
+        appBar: const CustomappbarForStartingview(),
 
-    ),
+        bottomNavigationBar: CurvedNavigationBar(
+          items: items,
+          height: 50,
+          index: index,
+          backgroundColor: Colors.transparent,
+          onTap: (value) {
+            setState(() {
+              index = value;
+            });
+          },
+        ),
+      ),
     );
   }
 }
