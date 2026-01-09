7825c29d48e9642266ca99cc9c8490d67bf3db75
halauser <ha00la2002@gmail.com>
2026-01-10 02:45:05 +0300

    edit_

--- lib/views/warning_msg/view_emptyReservationView.dart
+++ lib/views/warning_msg/view_emptyReservationView.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:yusur_app/utils/app_icons.dart';
+import 'package:yusur_app/widget/back_navigation_app.dart';
 import './widget/coustom_emptystate.dart';
 
 class EmptyReservationView extends StatelessWidget {
@@ -8,6 +9,7 @@ class EmptyReservationView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      appBar: BackNavigationAppBar(title: "طلبات الحجوزات"),
       body: EmptyStateBody(
         title: "لا يوجد  حجوزات بعد ",
         subTitle: "قم بحجز دورة لتظهر هنا ",
