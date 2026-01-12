ff1b027a21128ac4f60eb9d0855a28e77357f72b
khaledmamdooh <khaledalkady779@gmail.com>
2026-01-12 13:12:21 +0600

    Implement API consumer methods and error handling for Dio integration

---
+++ lib/api/api_interceptors.dart
@@ -0,0 +1,14 @@
+import 'package:dio/dio.dart';
+
+import 'end_points.dart';
+
+class ApiInterceptor extends Interceptor {
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    options.headers[ApiKey.token] = '<API_TOKEN>';
+    // CacheHelper().getData(key: ApiKey.token) != null
+    //     ? 'FOODAPI ${CacheHelper().getData(key: ApiKey.token)}'
+    //     : null;
+    super.onRequest(options, handler);
+  }
+}
