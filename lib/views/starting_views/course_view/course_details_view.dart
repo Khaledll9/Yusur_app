fa4bcaff8038dff7aefa8add35ca39f2e91c9fb8
Eng-YousefAlazhari <aforsite79@gmail.com>
2026-01-12 19:17:20 +0300

    Refactor CourseDetailsView layout to streamline image display

--- lib/views/starting_views/course_view/course_details_view.dart
+++ lib/views/starting_views/course_view/course_details_view.dart
@@ -15,12 +15,10 @@ class CourseDetailsView extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 16.0),
         child: Column(
           children: [
-            Expanded(
-              child: ClipRRect(
-                borderRadius: BorderRadiusGeometry.circular(10),
+            ClipRRect(
+              borderRadius: BorderRadiusGeometry.circular(10),
 
-                child: Image.asset(course.photoPath!),
-              ),
+              child: Image.asset(course.photoPath!),
             ),
             const SizedBox(height: 20),
 
